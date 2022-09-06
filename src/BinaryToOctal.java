df006910de6de1d3112a86e05e41f9f3e5f1e0ab
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-06 17:55:26 +0530

    modified:logic for binary to octal

---
+++ src/BinaryToOctal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class BinaryToOctal {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a binary number : ");
+        int binary = sc.nextInt();
+        int decimal = 0;
+        int n = 0;
+        while (binary > 0) {
+            int a = binary % 10;
+            decimal += a * Math.pow(2, n);
+            binary = binary / 10;
+            n++;
+        }
+        int octal[] = new int[20];
+        int i = 0;
+        while (decimal > 0) {
+            int r = decimal % 8;
+            octal[i++] = r;
+            decimal = decimal / 8;
+        }
+        System.out.print("Octal number : ");
+        for (int j = i - 1; j >= 0; j--)
+            System.out.print(octal[j]);
+    }
+}
+
