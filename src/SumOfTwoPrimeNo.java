3ac395f962256614a3abad9be9106a4d9108b218
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-09 18:08:52 +0530

    modified:logic for sum of two prime numbers

---
+++ src/SumOfTwoPrimeNo.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class SumOfTwoPrimeNo {
+    public static void main(String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number : ");
+            int number = sc.nextInt();
+            int x = 0;
+            for(int i = 2 ; i <= number/2 ; i++)
+            {
+                if(prime_or_not(i) == 1)
+                {
+                    if(prime_or_not(number-i) == 1)
+                    {
+                        System.out.println(number+ " = "+i+" + "+(number-i));
+                        x = 1;
+                    }
+                }
+            }
+            if(x == 0)
+                System.out.println(+number+" cant be expressed as a sum of two prime numbers");
+        }
+        //function for checking number is prime or not
+        public static int prime_or_not(int n)
+        {
+            int c = 1;
+            for(int i = 2 ; i < n ; i++)
+            {
+                if(n % i == 0)
+                {
+                    c = 0;
+                    break;
+                }
+            }
+            return c;
+        }
+    }
+
