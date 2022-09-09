c78f88eb3b10d56f545fa0f6c4f996cb4d07f614
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-09 18:41:29 +0530

    modified:logic for 1 to 100 prime no

---
+++ src/PrimeNoOneToHundred.java
@@ -0,0 +1,26 @@
+public class PrimeNoOneToHundred {
+    public static void main(String[] args) {
+        int a=1,b=100;
+        for(int i=a;i<=b;i++){
+            if(checkPrime(i)){
+                System.out.print(i+" " );
+            }
+        }
+    }
+    public static boolean checkPrime(int num){
+
+        if(num<2){
+            return false;
+        }
+        else{
+            int x= num/2;
+            for(int i=2;i<x;i++){
+                if(num%i==0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
